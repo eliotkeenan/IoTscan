6ce27e672e5cbf7da4e7e7698934668e9d255cb7
nobody615 <eliotknn@gmail.com>
2021-03-02 17:29:03 +0000

    .

--- iotscan/scan.py
+++ iotscan/scan.py
@@ -2,9 +2,16 @@ import nmap
 
 nm = nmap.PortScanner()
 
-nm.scan('127.0.0.1')
+# Scan virtualised network
+nm.scan("10.0.2.19", arguments="-A -T5")
 
 for host in nm.all_hosts():
-    print('----------------------------------------------------')
-    print('Host : %s (%s)' % (host, nm[host].hostname()))
+    print('Host : %s' % host)
     print('State : %s' % nm[host].state())
+
+    for proto in nm[host].all_protocols():
+        print("\nProtocol: %s" % proto)
+
+        lport = nm[host][proto].keys()
+        for port in lport:
+            print("port: %s\tstate: %s" % (port, nm[host][proto][port]['state']))
