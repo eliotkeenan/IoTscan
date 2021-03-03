79ea038f47cbbf9468a48b08de89793f3a08d966
nobody615 <eliotknn@gmail.com>
2021-03-03 20:51:49 +0000

    started doing code for handling os data (name, accuracy). TODO: get type

--- iotscan/scan.py
+++ iotscan/scan.py
@@ -2,23 +2,34 @@ import nmap
 
 nm = nmap.PortScanner()
 
-# Scan IoTGoat
+# Scan IoTGoat instance
 # use -T5 for speed (add aggression level later as optional flag)
-nm.scan("10.0.2.19", arguments="-A -O -T5")
+# needs to be run as root due to -O
+nm.scan("10.0.2.3", arguments="-A -O -T5")
 
 for host in nm.all_hosts():
+    osdata = []
+    for i in range(0, len(nm[host]['osmatch'])):
+        # get top three results
+        if (i > 2):
+            break
+
+        osdata.append(nm[host]['osmatch'][i])
+
     print('Host : %s' % host)
     print('State : %s' % nm[host].state())
 
-    # found something that kind of works for the operating system detection
-    #for osmatch in nm[host]['osmatch']:
-    #print("name: {0}".format(osmatch['name']))
-    #print("type: {0}".format(osmatch['type']))
+    os_names = []
+    os_accuracy = []
+    for unpack_osdata in osdata:
+        for k, v in unpack_osdata.items():
+            if k == 'name':
+                os_names.append(v)
+            if k == 'accuracy':
+                os_accuracy.append(v)
 
-    # dictionary types within the list then more shit within the dictionaries
-    for osdata in nm[host]['osmatch']:
-        for k, v in osdata.items():
-            print(k, v)
+    print(os_names)
+    print(os_accuracy)
 
     for proto in nm[host].all_protocols():
         print("\nProtocol: %s" % proto)
