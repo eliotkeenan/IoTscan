7205ed2ad1edd02d7e1fbab23574e09272ab5ae0
nobody615 <eliotknn@gmail.com>
2021-03-04 14:38:21 +0000

    done 'type' as well inelegant way of doing things. using 3 lists whose indexes align. Definitely a better way of doing this

--- iotscan/scan.py
+++ iotscan/scan.py
@@ -11,25 +11,33 @@ for host in nm.all_hosts():
     osdata = []
     for i in range(0, len(nm[host]['osmatch'])):
         # get top three results
-        if (i > 2):
+        if i > 2:
             break
 
         osdata.append(nm[host]['osmatch'][i])
+
+
+    print(osdata)
 
     print('Host : %s' % host)
     print('State : %s' % nm[host].state())
 
     os_names = []
     os_accuracy = []
+    os_types = []
     for unpack_osdata in osdata:
         for k, v in unpack_osdata.items():
             if k == 'name':
                 os_names.append(v)
             if k == 'accuracy':
                 os_accuracy.append(v)
+            if k == 'osclass':
+                print(v[0])
+                os_types.append(v[0]['type'])
 
     print(os_names)
     print(os_accuracy)
+    print(os_types)
 
     for proto in nm[host].all_protocols():
         print("\nProtocol: %s" % proto)
